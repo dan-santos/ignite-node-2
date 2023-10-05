6dcee940856488c2c07424aed5f082244b7e8344
Dan <santos.danielfd@gmail.com>
2023-10-04 22:57:53 -0300

    setup for deploy

--- src/env/index.ts
+++ src/env/index.ts
@@ -10,9 +10,9 @@ if (process.env.NODE_ENV === 'test') {
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'prd']).default('prd'),
   DATABASE_URL: z.string(),
-  DATABASE_CLIENT: z.string(),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_MIGRATIONS_PATH: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
   HOST: z.string().default('http://localhost'),
 });
 
